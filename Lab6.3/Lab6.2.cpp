e3b04cf4c41ece288d0c1a133eb37b578f9d2dd9
MarkStadnytskyi <72573640+MarkStadnytskyi@users.noreply.github.com>
2020-11-16 23:41:07 +0200

    Add files via upload

---
+++ Lab6.3/Lab6.2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+void create(int* t, const int n, const int min, const int max)
+{
+    for (int i = 0; i < n; i++)
+        t[i] = min + rand() % (max - min + 1);
+}
+
+void modify(int* t, const int n)
+{
+    for (int i = 0; i < n; i++)
+        if (t[i] % 2 != 0)
+            t[i] = 0;
+
+
+}
+
+void print(int* t, const int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << " " << t[i] << "; ";
+    cout << endl;
+}
+
+int sum(int* t, const int n)
+{
+    int S = 0;
+    for (int i = 0; i < n; i++)
+        if (t[i] % 2 == 0)
+            S += t[i];
+    return S;
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int n = 25;
+    int t[n];
+    int min = -11;
+    int max = 13;
+    create(t, n, min, max);
+    cout << "Масив із рандомних чисел:";
+    print(t, n);
+    cout << "Модифікований масив: ";
+    modify(t, n);
+    print(t, n);
+    cout << "Сума елементів масиву: " << sum(t, n) << endl;
+
+
+    return 0;
+}
